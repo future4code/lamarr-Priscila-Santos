4e27e6f9d8940c227a6e9f43dc90a339d147fb17
priscilasds28 <prifs26@gmail.com>
2022-04-12 22:49:00 -0300

    exercico variavel

---
+++ modulo-1/variavei/index.js
@@ -0,0 +1,27 @@
+//Exercicio 3
+let 6 = prompot("Quantas horas você trablha por dia?")
+let 60 = prompot("Quanto você recebe por dia?")
+alert(´Voce recebe ${t/p} por hora´)
+
+//Exercicio 1.1
+
+const nome = "Priscila"
+const idade = 28
+
+ typeof nome // string
+ typeof idade // number
+console.log("Qual meu nome?", "Qual minha idade?")
+
+typeof nome = String
+typeof idade = Number
+//Não notei diferença, fiquei confusa
+
+console.log("olá" nome, "você tem" idade "anos" )
+ 
+//Exercicio 2.2
+let usoCalcaAzul = não
+let tenhoCabeloCacheado = sim 
+let tenhoFilho = sim 
+ console.log("você está usando uam calça azul hoje?")
+ console.log("tem cachos?")
+ console.log("tem filhos?")
