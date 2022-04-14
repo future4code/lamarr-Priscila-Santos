91ad67138856527e535ac495ecca6bbb18a2c2c2
priscilasds28 <prifs26@gmail.com>
2022-04-14 20:13:54 -0300

    exercico-variaveis

--- modulo-1/variavei/index.js
+++ modulo-1/variavei/index.js
@@ -1,27 +1,71 @@
-//Exercicio 3
-let 6 = prompot("Quantas horas você trablha por dia?")
-let 60 = prompot("Quanto você recebe por dia?")
-alert(´Voce recebe ${t/p} por hora´)
+// Exercicio 1.1
+console.log(10) 
+console.log(10,10) 
 
-//Exercicio 1.1
+//Exercico 2.2
+// dá erro na execução porque é atribuido dois valores para b, 10 e 20.
 
-const nome = "Priscila"
-const idade = 28
+//Exercico 3.3
+let horaTrabalhadaPorDia = prompt("Quantas horas você trabalha por dia?")
+let diariaPorDia = prompt("Quanto você recebe por dia?")
 
- typeof nome // string
- typeof idade // number
-console.log("Qual meu nome?", "Qual minha idade?")
+//Exercicio 1.2
+let nome =  null
+let idade = null 
 
-typeof nome = String
-typeof idade = Number
-//Não notei diferença, fiquei confusa
+console.log(typeof nome)
+console.log(typeof idade)
 
-console.log("olá" nome, "você tem" idade "anos" )
- 
-//Exercicio 2.2
-let usoCalcaAzul = não
-let tenhoCabeloCacheado = sim 
-let tenhoFilho = sim 
- console.log("você está usando uam calça azul hoje?")
- console.log("tem cachos?")
- console.log("tem filhos?")
+// não há valor na variavel então está indefinida.
+
+nome = prompt("Qual o seu nome?")
+idade = prompt("Qual a sua idade?")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+/*A primeira a variavel ficou como object porque não estava definida
+ e após ser definida deu o tipo que é string*/
+
+ console.log( "olá, ", nome, "você tem, " ,idade, "anos?")
+
+ //Exercico 2.2
+
+ let cachorro = prompt("Você tem cachorro? Sim/Não")
+ let carro = prompt("Você tem carro? Sim/Não")
+ let chocolate = prompt("Você gosta de chocolate? Sim/Não")
+
+ let rCachorro = (cachorro ==="Sim")
+ let rCarro = (carro === "Sim")
+ let rChocolate = (chocolate === "Sim")
+
+ console.log(rCachorro, rCarro, rChocolate)
+
+ //Exercicio 3.3
+let a = 10
+let b = 25
+let c = null
+
+ a = 10
+ b = 25
+ c = null
+ c = b
+ b = a
+ a = c 
+ console.log( a, b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
